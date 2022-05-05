a06529ec904c257210465f307e6851ea8420209e
Enes Turgut <burdaenesvar@gmail.com>
2022-05-06 02:02:41 +0300

    html sayfaları düzenleme

--- iletisim.php
+++ iletisim.php
@@ -7,6 +7,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/iletişim.css">
+    <script src="JS/all.min.js"></script><!--iconsınıfı-->
     <title>İletişim</title>
   </head>
   <body style="margin: 0px 10px;">
@@ -22,11 +23,10 @@
     header("Location: iletişim.html"); 
   }  
   ?>
-
     <!--Navbar-Start-->
     <div id="alert"></div>
     <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #dbd5d5;">
         <div class="container-fluid">
           <a style="color: #330000;" class="navbar-brand" href="hakkında.html">EBT</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -62,48 +62,111 @@
      <div class="container">
          <div class="row mt-5">
            <div class="col-sm-2"></div>
-             <div class="col-md-8 col-sm-8">
-               <h2>FORM BİLGİLERİ</h2>
-                <table class="formbilgi" border="1px">
-                    <tr>
-                        <th>AD</th>
+           <div class="col-md-8 col-sm-8">
+               <h2 style="color: #330000;">FORM BİLGİLERİ</h2>
+                <table class="table table-striped formbilgi">
+                  <tbody>
+                  <tr>
+                        <th scope="row">AD</th>
                         <td>:</td>
                         <td><?php echo $_POST['ad']?></td>
                     </tr>
                     <tr>
-                        <th>E-MAİL</th>
+                        <th scope="row">E-MAİL</th>
                         <td>:</td>
                         <td><?php echo $_POST['email']?></td>
                     </tr>
                     <tr>
-                        <th>CİNSİYET</th>
+                        <th scope="row">CİNSİYET</th>
                         <td>:</td>
                         <td><?php echo $_POST['cinsiyet']?></td>
                     </tr>
                     <tr>
-                        <th>EnBeğenilenSayfa</th>
+                        <th scope="row">EnBeğenilenSayfa</th>
                         <td>:</td>
                         <td><?php echo $_POST['sayfa']?></td>
                     </tr>
                     <tr>
-                        <th>PUAN</th>
+                        <th scope="row">PUAN</th>
                         <td>:</td>
                         <td><?php echo $_POST['puan']?></td>
                     </tr>
                     <tr>
-                        <th>MESAJ</th>
+                        <th scope="row">MESAJ</th>
                         <td>:</td>
                         <td><?php echo $_POST['mesaj']?></td>
-                    </tr>                  
+                    </tr>         
+                  </tbody>         
                 </table>
              </div>
          </div>
+
      </div>
+
+    <!--Footer-Start-->
+   <footer>
+    <div class="container">
+      <div class="row mt-3">
+       <div class="col-md-4 col-sm-4">
+
+       </div>
+       <div class="col-md-4 col-sm-4 mt-3">
+         <h2 style="margin-left: 8%;">İletişim</h2>
+         <ul class="footer-list">
+           <li>
+             <span>
+               <a style="color:#C13584;" href="https://www.instagram.com/?hl=tr" target="_blank">
+                 <i class="fa-brands fa-instagram fa-xl"></i>
+               </a>
+             </span>
+           </li>
+           
+           <li>
+             <span style="color: Dodgerblue;">
+               <a href="https://twitter.com/Enesbtg" target="_blank">
+                 <i class="fa-brands fa-twitter fa-xl"></i>
+               </a> 
+             </span>
+           </li>
+           <li>
+             <span>
+               <a style="color:white;">
+                 <i class="fas fa-phone"></i>
+                 +5435514080
+               </a>         
+             </span>
+           </li>
+           <li>
+             <span>
+               <a style="color:white;">
+                 <i class="far fa-envelope"></i>
+                 burdaenesvar@gmail.com
+               </a>         
+             </span>
+           </li>
+         </ul>
+       </div>
+       <div class="col-md-4 col-sm-4 mt-3">
+         <h2>Faydalı Linkler</h2>
+         <ul class="footer-list">
+           <li><a href="hakkında.html">Hakkında</a> </li>
+           <li><a href="ilgialanlarım.html">İlgi Alanlarım</a></li>
+           <li><a href="şehrim.html">Şehrim</a></li>
+           <li><a href="takımımız.html">Takımımız</a></li>
+           <li><a href="özgeçmiş.html">Özgeçmiş</a></li>
+           <li><a href="iletişim.html">İletişim</a></li>
+         </ul>
+       </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 col-sm-12">
+          <p style="text-align: center;">©2022 Enes Buğra Turğut</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--Footer-End-->
      
-
-
-
-
      <script src="JS/iletişim.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
